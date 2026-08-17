e64ad1b07ca68ee7e5b76fd00ce55a0f69ae9cd6
samansamanyasin01 <samansamanyasin01@gmail.com>
2026-08-17 23:21:52 +0500

    more configuration

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,8 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true, isolatedModules: true }],
   },
   testMatch: ['**/tests/**/*.test.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  // Map .js extension imports in TS source files back to .ts/.js resolution
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
